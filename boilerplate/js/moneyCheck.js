a2ba3949b3e2382fa9a1648f439982dff957f936
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2018-08-21 06:27:04 +0300

    Little patch

--- boilerplate/js/moneyCheck.js
+++ boilerplate/js/moneyCheck.js
@@ -4,6 +4,6 @@ var bread = +prompt("Сколько вы купили батонов хлеба?
 var totalApplePrice = apple*prompt("Сколько стоит одно яблоко?");
 var totalBreadPrice = bread*prompt("Сколько стоит один батон хлеба?");
 var check = money >= totalApplePrice+totalBreadPrice;
-document.body.write = check;
+document.write(check);
 
 
