20f9f6667217e0bdc2a45657132c49cb09e2aa3c
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2018-09-03 22:01:25 +0300

    2 level array walkthrough with for-loop

---
+++ 2 level array walkthrough with for-loop/script.js
@@ -0,0 +1,17 @@
+let names = [
+	[
+		['куры', 'гуси', 'павлины'],
+		['сокол', 'орел', 'соловей']
+	],
+	[
+		['собака', 'кошка'],
+		['обезьяна', 'рысь']
+	]
+];
+
+//document.body.innerHTML = names;
+for (group of names) {
+  	for (name of group) {
+	  	console.log(name);
+    }
+}
