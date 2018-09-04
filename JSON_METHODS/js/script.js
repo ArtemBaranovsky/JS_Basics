bde027ae34b61d4de179e6a59fb14bc1ad09f8ac
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2018-09-04 07:55:49 +0300

    JSON basics demo

---
+++ JSON_METHODS/js/script.js
@@ -0,0 +1,7 @@
+// string to JSON parse
+// const apiString = '[ { "city" : "Moscow", "temp" : 10, "pressure" : 740 }, { "city" : "Severodonetsk", "temp" : 15, "pressure" : 748 }, { "city" : "Deli", "temp" : 30, "pressure" : 750 }]';
+// const DATA1 = JSON.parse(apiString);
+// console.log(DATA1);
+
+const apiString = JSON.stringify(DATA);
+console.log(apiString);
